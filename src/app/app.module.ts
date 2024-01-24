b56677652c48dfa9a19c2c06aa251d8de8b45a9f
Arun-M-2000 <arunm101220@gmail.com>
2024-01-24 15:06:00 +0530

    gg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,24 +12,21 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 
-import { FormsModule } from '@angular/forms';
 
 
 
 
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatNativeDateModule } from '@angular/material/core';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { CommonModule } from '@angular/common';
 
-import { ReactiveFormsModule } from '@angular/forms';
 
-import { FormsModule } from '@angular/forms';
 
 
 
@@ -63,16 +60,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
-    MatButtonModule,
+    // MatSlideToggleModule,
+    // MatButtonModule,
 
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSelectModule,
+    // MatFormFieldModule,
+    // MatDatepickerModule,
+    // MatNativeDateModule,
+    // MatSelectModule,
     CommonModule,
- 
-      ReactiveFormsModule,
+   ReactiveFormsModule,
 
   ],
   providers: [],
